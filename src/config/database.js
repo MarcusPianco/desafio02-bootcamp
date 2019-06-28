1a6835e0c22da0bdfa54791dfef5aa1f2a8e82c0
marcus pianco <marcus.ufal@gmail.com>
2019-06-28 06:40:06 -0300

    Project Configuration

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialetc: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'desafio02',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
