a511d1185bf0533eae376e9d80aee57ee1e32016
marcus pianco <marcus.ufal@gmail.com>
2019-07-01 23:18:43 -0300

    Build validation resource strategy

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,24 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 import Profile from '../models/Profile';
 
 class UserController {
   async store(req, res) {
+    const scheme = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+      profile: Yup.string()
+        .required()
+        .max(2),
+    });
+
+    if (!(await scheme.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const userExist = await User.findOne({ where: { email: req.body.email } });
 
     if (userExist) {
@@ -24,16 +40,43 @@ class UserController {
   }
 
   async update(req, res) {
-    const userExist = await User.findByPk(req.userID);
+    const scheme = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      profile: Yup.string().max(2),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
 
-    if (!userExist) {
-      return res.status(400).json({ error: 'User does not exist' });
+    if (!(await scheme.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userID);
+
+    if (email !== user.email) {
+      const userExist = await User.findOne({ where: { email } });
+      if (userExist) {
+        return res.status(400).json({ error: 'User already exist!!' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
     }
 
     // eslint-disable-next-line camelcase
-    const { id, email, profile_id, name } = await userExist.update(req.body);
+    const { id, profile_id, name } = await user.update(req.body);
 
-    return res.json({ id, email, profile_id, name });
+    return res.json({ id, profile_id, name });
   }
 }
 export default new UserController();
